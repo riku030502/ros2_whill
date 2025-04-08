21c5c96912d5d3d324eceaa1e707068eb01e6572
riku030502 <maemukiriku@gmail.com>
2025-04-08 12:29:22 +0900

    gazeboにWHILLを表示

---
+++ whill_simulations/setup.py
@@ -0,0 +1,31 @@
+from glob import glob
+import os
+from setuptools import find_packages, setup
+
+
+
+package_name = 'whill_simulations'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # launch ディレクトリをインストール対象に追加！
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tequila',
+    maintainer_email='maemukiriku@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
